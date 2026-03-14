d3ffe9d864cffc9a4f7ff4e50a6e36c927bbd710
mikloshetzer-sketch <mikloshetzer@gmail.com>
2026-03-14 21:24:31 +0100

    Create rss_sources.py

---
+++ config/rss_sources.py
@@ -0,0 +1,40 @@
+# config/rss_sources.py
+
+RSS_SOURCES = [
+    {
+        "name": "Politico Europe",
+        "url": "https://www.politico.eu/feed/",
+        "type": "media",
+        "enabled": True,
+    },
+    {
+        "name": "Euractiv",
+        "url": "https://www.euractiv.com/feed/",
+        "type": "media",
+        "enabled": True,
+    },
+    {
+        "name": "EUobserver",
+        "url": "https://euobserver.com/rss",
+        "type": "media",
+        "enabled": True,
+    },
+    {
+        "name": "Reuters Europe",
+        "url": "https://www.reuters.com/world/europe/rss",
+        "type": "media",
+        "enabled": True,
+    },
+    {
+        "name": "BBC News Europe",
+        "url": "https://feeds.bbci.co.uk/news/world/europe/rss.xml",
+        "type": "media",
+        "enabled": True,
+    },
+    {
+        "name": "European Parliament News",
+        "url": "https://www.europarl.europa.eu/rss/en/news.xml",
+        "type": "institution",
+        "enabled": True,
+    },
+]
